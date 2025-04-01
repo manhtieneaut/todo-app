27ae9bb89c4b2d9ed28eccf04773d11b7c1fb9fe
Nguyen Manh Tien (CTV-CNTT) <tiennm1@kienlongbank.com>
2025-04-01 10:20:50 +0700

    Initial commit on dev branch

---
+++ src/component/header.tsx
@@ -0,0 +1,13 @@
+// components/header.tsx
+
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>My Todo App</h1>
+    </header>
+  );
+};
+
+export default Header;
