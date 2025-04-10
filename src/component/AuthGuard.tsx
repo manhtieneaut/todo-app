ac4a58b849b0538f57c4317d702e17b56b6d130e
Nguyen Manh Tien (CTV-CNTT) <tiennm1@kienlongbank.com>
2025-04-10 16:36:51 +0700

    update auth guard

--- src/component/AuthGuard.tsx
+++ src/component/AuthGuard.tsx
@@ -1,17 +1,28 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { supabase } from '@/lib/supabaseClient';
 
 export default function AuthGuard({ children }: { children: React.ReactNode }) {
+  const [loading, setLoading] = useState(true);
   const router = useRouter();
 
   useEffect(() => {
-    const token = localStorage.getItem('jwt_token');
-    if (!token) {
-      router.replace('/login');
-    }
+    const checkAuth = async () => {
+      const { data, error } = await supabase.auth.getSession();
+
+      if (error || !data.session) {
+        router.replace('/login');
+      } else {
+        setLoading(false);
+      }
+    };
+
+    checkAuth();
   }, []);
+
+  if (loading) return <div>Loading...</div>;
 
   return <>{children}</>;
 }
